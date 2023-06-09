8da5dce54fb05184f869018c464826922b77cd8b
yinghao.xu <yinghao.xu@trip.com>
2023-06-09 17:44:44 +0800

    feat: 上传自己的 docusaurus

---
+++ src/pages/my-markdown-page.md
@@ -0,0 +1,11 @@
+<!--
+ * @Author: yinghao.xu
+ * @Date: 2023-06-09 16:10:35
+ * @LastEditors: yinghao.xu
+ * @LastEditTime: 2023-06-09 16:10:49
+ * @Description: 
+ * 
+-->
+# My Markdown page
+
+This is a Markdown page
---
+++ src/pages/my-react-page.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: yinghao.xu
+ * @Date: 2023-06-09 16:04:52
+ * @LastEditors: yinghao.xu
+ * @LastEditTime: 2023-06-09 16:05:11
+ * @Description: 
+ * 
+ */
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>This is a React page</p>
+    </Layout>
+    // <div>
+    //   <h1>My React page</h1>
+    //   <p>This is a React page</p>
+    // </div>
+  );
+}
