4cb611fcb770eb264d33f5be0394baace366faef
yinghao.xu <yinghao.xu@trip.com>
2023-06-09 18:00:26 +0800

    feat: 修改配置文件的项目名称参数

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yinghaoX', // Usually your GitHub org/user name.
+  projectName: 'yinghaoX.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
